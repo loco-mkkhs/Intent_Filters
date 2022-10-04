1cf34b15f0f1b34366a58bbd2f84e01a7440e762
loco-mkkhs <105632704+loco-mkkhs@users.noreply.github.com>
2022-10-04 09:09:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/intent_filters/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.intent_filters
+/*
+import android.R
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var editText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+    }
+
+    var button: ComponentType = findViewById<View>(R.id.IdOfTheComponent) as ComponentType
+
+    fun search() {
+        val url = editText.text.toString()
+        val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(urlIntent)
+    }
+}
+*/
+ 
